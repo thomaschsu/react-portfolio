518664ce945498e71808fdf28bbe6a52bd3e9fae
Tom Hsu <thomaschsu@gmail.com>
2019-10-03 19:00:08 -0400

    Modified header

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,7 @@ class Header extends Component {
     render() {
         return(
             <header className="Header">
+                <h1 className="Header-heading">Hi! My name is Tom Hsu.</h1>
                 <img id="me" src="images/profile.jpg" alt="Picture of Tom Hsu"/>
             </header>
         )
