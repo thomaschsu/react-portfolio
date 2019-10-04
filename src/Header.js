7639643a0fbb87d6931b5ef5a3ea014b888e2de3
Tom Hsu <thomaschsu@gmail.com>
2019-10-03 20:03:53 -0400

    Modified header and navigation

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,13 @@ import './Header.scss'
 
 class Header extends Component {
     render() {
-        return(
+        return (
             <header className="Header">
-                <h1 className="Header-heading">Hi! My name is Tom Hsu.</h1>
-                <img id="me" src="images/profile.jpg" alt="Picture of Tom Hsu"/>
+                <div className="Header-about-box">
+                    <h1 className="Header-heading">Hi! My name is Tom Hsu.</h1>
+                    <img id="me" src="images/profile.jpg" alt="Picture of Tom Hsu" />
+                    <h2 className="Header-subtitle">I'm a front-end web developer</h2>
+                </div>
             </header>
         )
     }
