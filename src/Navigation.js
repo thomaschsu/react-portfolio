e0d5cc97939ad290e37e85d1ad32884778e697a1
Tom Hsu <thomaschsu@gmail.com>
2019-10-04 21:31:13 -0400

    Added blink animation

--- src/Navigation.js
+++ src/Navigation.js
@@ -13,10 +13,10 @@ class Navigation extends Component {
 
                     <nav className="Navigation__nav">
                         <ul className="Navigation__list">
-                            <li className="Navigation__item"><a href="#" className="Navigation__link"><span>01</span>Home</a></li>
-                            <li className="Navigation__item"><a href="#" className="Navigation__link"><span>02</span>Contact</a></li>
-                            <li className="Navigation__item"><a href="#" className="Navigation__link"><span>03</span>Portfolio</a></li>
-                            <li className="Navigation__item"><a href="#" className="Navigation__link"><span>04</span>Resume</a></li>
+                        <li className="Navigation__item"><a href="#" className="Navigation__link"><span>1</span><span className="Navigation-colon">:</span>Home</a></li>
+                        <li className="Navigation__item"><a href="#" className="Navigation__link"><span>2</span><span className="Navigation-colon">:</span>Contact</a></li>
+                        <li className="Navigation__item"><a href="#" className="Navigation__link"><span>3</span><span className="Navigation-colon">:</span>Portfolio</a></li>
+                        <li className="Navigation__item"><a href="#" className="Navigation__link"><span>4</span><span className="Navigation-colon">:</span>Resume</a></li>
                         </ul>
                     </nav>
             </div>
