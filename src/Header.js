0a96179ff83bfe8b7415ceda4a9fdf0396c202d2
Tom Hsu <thomaschsu@gmail.com>
2019-10-09 10:52:02 -0400

    Changed header.js to bem

--- src/Header.js
+++ src/Header.js
@@ -5,10 +5,10 @@ class Header extends Component {
     render() {
         return (
             <header className="Header">
-                <div className="Header-about-box">
-                    <h1 className="Header-heading highlight-hover text-shadow">Hi! My name is <span className="Header-name">Tom Hsu</span></h1>
+                <div className="Header__about">
+                    <h1 className="Header__heading highlight-hover text-shadow">Hi! My name is <span className="Header__name">Tom Hsu</span></h1>
                     <img id="me" src="images/profile.jpg" alt="Picture of Tom Hsu" />
-                    <h2 className="Header-subtitle text-shadow">I'm a front-end web developer</h2>
+                    <h2 className="Header__subtitle text-shadow">I'm a front-end web developer</h2>
                 </div>
             </header>
         )
