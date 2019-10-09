49767522f2447af81a2b36dd38d7bd623322f3ca
Tom Hsu <thomaschsu@gmail.com>
2019-10-09 13:59:50 -0400

    Added icons_bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss'
 import Navigation from '../Navigation/Navigation'
 import Header from '../Header/Header'
+import Icons_bar from '../Icons-bar/Icons_bar'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Header />
+      <Icons_bar />
     </div>
   );
 }
