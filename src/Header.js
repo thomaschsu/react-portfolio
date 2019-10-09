73c9eb87c7d7c47a47c29958aacd594304e7fff2
Tom Hsu <thomaschsu@gmail.com>
2019-10-09 10:53:27 -0400

    Changed about-box

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     render() {
         return (
             <header className="Header">
-                <div className="Header__about">
+                <div className="Header__about-box">
                     <h1 className="Header__heading highlight-hover text-shadow">Hi! My name is <span className="Header__name">Tom Hsu</span></h1>
                     <img id="me" src="images/profile.jpg" alt="Picture of Tom Hsu" />
                     <h2 className="Header__subtitle text-shadow">I'm a front-end web developer</h2>
