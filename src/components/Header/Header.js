cd2efa239070bbcbee133311e0ffc3dcead6ff62
Tom Hsu <thomaschsu@gmail.com>
2019-10-09 12:34:41 -0400

    Fixed redundant alt error

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
             <header className="Header">
                 <div className="Header__about-box">
                     <h1 className="Header__heading highlight-hover text-shadow">Hi! My name is <span className="Header__name">Tom Hsu</span></h1>
-                    <img id="me" src="images/profile.jpg" alt="Picture of Tom Hsu" />
+                    <img id="me" src="images/profile.jpg" alt="Tom Hsu" />
                     <h2 className="Header__subtitle text-shadow">I'm a front-end web developer</h2>
                 </div>
             </header>
