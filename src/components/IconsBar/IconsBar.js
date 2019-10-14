013a4b56e0ed97a1c10574ce5978c92ddf13ef96
Tom Hsu <thomaschsu@gmail.com>
2019-10-14 12:33:46 -0400

    Edited icons bar to pascal

--- src/components/IconsBar/IconsBar.js
+++ src/components/IconsBar/IconsBar.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react'
 import './IconsBar.scss'
 
-class Icons_bar extends Component {
+class IconsBar extends Component {
     render() {
         return (
-            <div className="Icons_bar">
-                <div className="Icons_bar--icons-container">
-                    <div className="Icons_bar--box">
+            <div className="IconsBar">
+                <div className="IconsBar--icons-container">
+                    <div className="IconsBar--box">
                         <h2>Experienced</h2>
                         Proven experience in developing user-friendly websites and web applications.
                     </div>
-                    <div className="Icons_bar--box">
+                    <div className="IconsBar--box">
                         <h2>Skills</h2>
                         Well-versed in JavaScript, HTML, SCSS, CSS, Node, jQuery, MySQL and more.
                     </div>
-                    <div className="Icons_bar--box">
+                    <div className="IconsBar--box">
                         <h2>Debugger</h2>
                         A proactive professional who enjoys systematically testing websites for bugs and debugging errors.
                     </div>
@@ -24,4 +24,4 @@ class Icons_bar extends Component {
     }
 }
 
-export default Icons_bar
+export default IconsBar
