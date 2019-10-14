b95d90ada4902788edc5929b7094514deb710373
Tom Hsu <thomaschsu@gmail.com>
2019-10-14 12:52:41 -0400

    Added icons to iconbar

--- src/components/IconsBar/IconsBar.js
+++ src/components/IconsBar/IconsBar.js
@@ -7,14 +7,17 @@ class IconsBar extends Component {
             <div className="IconsBar">
                 <div className="IconsBar--icons-container">
                     <div className="IconsBar--box">
+                        <h1><i class="fas fa-code-branch"></i></h1>
                         <h2>Experienced</h2>
                         Proven experience in developing user-friendly websites and web applications.
                     </div>
                     <div className="IconsBar--box">
+                        <h1><i class="fas fa-book"></i></h1>
                         <h2>Skills</h2>
                         Well-versed in JavaScript, HTML, SCSS, CSS, Node, jQuery, MySQL and more.
                     </div>
                     <div className="IconsBar--box">
+                        <h1><i class="fas fa-bug"></i></h1>
                         <h2>Debugger</h2>
                         A proactive professional who enjoys systematically testing websites for bugs and debugging errors.
                     </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import '@fortawesome/fontawesome-free/css/all.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
