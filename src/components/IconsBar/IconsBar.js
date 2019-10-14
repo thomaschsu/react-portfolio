10e6b64a8c41e5c8385d412aed25ea4b4e3c2229
Tom Hsu <thomaschsu@gmail.com>
2019-10-14 19:51:17 -0400

    Edited header and iconsbar / changed some colors

--- src/components/IconsBar/IconsBar.js
+++ src/components/IconsBar/IconsBar.js
@@ -7,18 +7,18 @@ class IconsBar extends Component {
             <div className="IconsBar">
                 <div className="IconsBar--icons-container">
                     <div className="IconsBar--box">
-                        <h1><i class="fas fa-code-branch"></i></h1>
-                        <h2>Experienced</h2>
+                        <h1><i className="fas fa-code-branch"></i></h1>
+                        <h2 className="IconsBar__h2-1">Experienced</h2>
                         Proven experience in developing user-friendly websites and web applications.
                     </div>
                     <div className="IconsBar--box">
-                        <h1><i class="fas fa-book"></i></h1>
-                        <h2>Skills</h2>
+                        <h1><i className="fas fa-book"></i></h1>
+                        <h2 className="IconsBar__h2-2">Skills</h2>
                         Well-versed in JavaScript, HTML, SCSS, CSS, Node, jQuery, MySQL and more.
                     </div>
                     <div className="IconsBar--box">
-                        <h1><i class="fas fa-bug"></i></h1>
-                        <h2>Debugger</h2>
+                        <h1><i className="fas fa-bug"></i></h1>
+                        <h2 className="IconsBar__h2-3">Debugger</h2>
                         A proactive professional who enjoys systematically testing websites for bugs and debugging errors.
                     </div>
                 </div>
