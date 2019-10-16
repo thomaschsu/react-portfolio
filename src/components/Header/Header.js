90d2ea3196126b53cca2cb1bb0b3b5d7793d7a27
Tom Hsu <thomaschsu@gmail.com>
2019-10-16 13:08:44 -0400

    Fixed navigation using state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     render() {
         return (
             <header className="Header">
-                <video class="hero-video" autoPlay muted loop>
+                <video className="hero-video" autoPlay muted loop>
                     <source src="videos/hero.mp4" type="video/mp4"></source>
                     <source src="videos/video.webm" type="video/webm"></source>
                     Your browser is not supported. Please use a newer browser!
@@ -14,9 +14,9 @@ class Header extends Component {
                     <h1 className="Header__heading highlight-hover text-shadow">Hi! My name is <span className="Header__name">Tom Hsu</span></h1>
                     <img id="me" src="images/profile.jpg" alt="Tom Hsu" />
                     <h2 className="Header__subtitle text-shadow">I'm a front-end web developer.</h2>
-                    <a href="https://www.linkedin.com/in/tomchsu/"><i class="fab fa-linkedin Header__icons"></i></a>
-                    <a href="https://github.com/thomaschsu/"><i class="fab fa-github-alt Header__icons"></i></a>
-                    <a href="mailto:thomaschsu@gmail.com"><i class="fas fa-envelope-open Header__icons"></i></a>
+                    <a href="https://www.linkedin.com/in/tomchsu/"><i className="fab fa-linkedin Header__icons"></i></a>
+                    <a href="https://github.com/thomaschsu/"><i className="fab fa-github-alt Header__icons"></i></a>
+                    <a href="mailto:thomaschsu@gmail.com"><i className="fas fa-envelope-open Header__icons"></i></a>
                 </div>
             </header>
         )
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react'
 import './Navigation.scss'
 
 class Navigation extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { 
+            active: false
+        }
+    }
     render() {
         return(
             <div className="Navigation">
-                <input type="checkbox" className="Navigation__checkbox" id="navi-toggle" />
+                <input type="checkbox" className={`Navigation__checkbox`} id="navi-toggle" onClick={() => this.setState({ active: !this.state.active })} />
                     <label htmlFor="navi-toggle" className="Navigation__button">
                         <span className="Navigation__icon">&nbsp;</span>
                     </label>
                     <div className="Navigation__background">&nbsp;</div>
 
                     <nav className="Navigation__nav">
-                        <ul className="Navigation__list">
+                    <ul className={`Navigation__list ${this.state.active && 'Navigation__display'}`}>
                         <li className="Navigation__item"><a href="#" className="Navigation__link">Home</a></li>
                         <li className="Navigation__item"><a href="#" className="Navigation__link">Contact</a></li>
                         <li className="Navigation__item"><a href="#" className="Navigation__link">Portfolio</a></li>
