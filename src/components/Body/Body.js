6e8d2f82dc5e0a0c38fc827372a66f900b5434fa
Tom Hsu <thomaschsu@gmail.com>
2019-10-20 20:53:05 -0400

    Modified body text

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -6,10 +6,10 @@ class Body extends Component {
         return (
             <div className="Body__container">
             <h2 className="Body__title">About Me</h2>
-                <p className="Body__paragraph">Tom is an experienced Front-End Web Developer with a proven background in developing user-friendly websites and web applications. He is familiar with full stack development and is well-versed in various programming languages including JavaScript, HTML, SCSS, CSS, Node, and more.</p>
-                <p className="Body__paragraph">Tom graduated from Rutgers University with a major in Information Technology. After graduation, he took a lead role in the production and maintenance of a front-end Magento and WordPress company website. This included implementing a PunchOut catalog, systematically testing the website for bugs, and managing the backend Magento catalog. Tom was also responsible for building and managing graphics for the front-end. He also provided client services in a massive company project that required imaging thousands of laptops and desktops for Elizabeth school districts all within tight time constraints.</p>
+                <p className="Body__paragraph">Tom is an experienced Front-End Web Developer with a proven background in developing user-friendly websites and web applications. He is familiar with full stack development and is well-versed in JavaScript, HTML, SCSS, CSS, Node, and more.</p>
+                <p className="Body__paragraph">Tom graduated from Rutgers University with a major in Information Technology. After graduation, he took a lead role in the production and maintenance of a front-end Magento and WordPress company website. This included implementing a PunchOut catalog, systematically testing the website for bugs, and managing the backend Magento catalog. Tom was also responsible for building and managing graphics for the front-end.</p>
 
-                <p className="Body__paragraph">On his free time, he likes to take care of his dog (Cocoa), play video games, watch movies / TV shows / anime, and enjoys exercising. He is knowledgeable in headphones - music has always been a big part of his life. He likes to listen to EDM, Hip-Hop, Rap, Pop, Rock, and occasionally classical. Tom also enjoys reading about car news.</p>
+                <p className="Body__paragraph">On his free time, he likes to take care of his dog (Cocoa), play video games, watch movies / TV shows, and enjoys exercising. He is knowledgeable in headphones - music has always been a big part of his life.</p>
             </div>
         )
     }
